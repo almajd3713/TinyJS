1d194f386bfc4ac825951ac08e7279648c2783d7
almajd3713 <almajd3713@gmail.com>
2025-07-23 11:02:26 +0400

    add simple grammer rules

---
+++ grammer_rules.py
@@ -0,0 +1,67 @@
+
+
+grammer_rules: dict[str, list[str]] = {
+    # Variable values
+    "VARIABLE": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    "DIGIT": "To be populated by code",
+    
+    # Arithmetics and operators
+    "ARITHMETIC_OPERATOR": ["+", "-", "*", "/", "%", "**"],
+    "RELATIONAL_OPERATOR": ["===", "!=", "<", ">", "<=", ">="],
+    "LOGICAL_OPERATOR_PREFIX": ["&&", "||"],
+    "LOGICAL_OPERATOR_INFIX": ["&", "|"],
+    "LOGICAL_OPERATOR": ["LOGICAL_OPERATOR_PREFIX", "LOGICAL_OPERATOR_INFIX"],
+    "OPERATOR": ["ARITHMETIC_OPERATOR"],
+
+    # Formatting
+    "NEW_LINE": ["\n"],
+    "TAB": ["\t"],
+    "PARENTHESIS_OPEN": ["("],
+    "PARENTHESIS_CLOSE": [")"],
+    "EQUALS": ["="],
+    "COLON": [":"],
+    "COMMA": [","],
+    "SEMICOLON_TERM": [";"],
+    "SEMICOLON": ["SEMICOLON", ""],
+
+    # Keywords
+    "VAR": ["var"],
+    "LET": ["let"],
+    "DEFINE_VAR": ['VAR', 'LET'],
+    "IF": ["if"],
+    "ELSE": ["else"],
+    "FOR": ["for"],
+    "WHILE": ["while"],
+    "OF": ["of"],
+    
+    # Terms and Expressions
+    "TERM": ["EXPRESSION_IDENTIFIER", "DIGIT"], # Expression identifier for existing variables, else pick a random digit
+    "EXPRESSION": ['TERM SPACE OPERATOR SPACE TERM SEMICOLON'],
+    "EXPRESSION_ENCLOSED": ['PARENTHESIS_OPEN EXPRESSION PARENTHESIS_CLOSE'],
+    "EXPRESSION_DISPLAY": [
+        'EXPRESSION_IDENTIFIER SPACE OPERATOR SPACE EXPRESSION_IDENTIFIER SEMICOLON', 
+        'EXPRESSION_IDENTIFIER SPACE OPERATOR SPACE DIGIT'
+    ],
+    
+    "IDENTIFIER_INITIALIZATION": ["INDENTIFIER_INITIALIZATION INITIALIZATION SEMICOLON", "INITIALIZATION SEMICOLON"],
+    "INITIALIZATION": ['VARIABLE SPACE EQUALS SPACE DIGIT SEMICOLON NEW_LINE'],
+    
+    "SIMPLE_ARITHMETIC_EXPRESSION": [
+        'SIMPLE_ARITHMETIC_EXPRESSION ARITHMETIC_OPERATOR ENCLOSED_EXPRESSION',
+        'ENCLOSED_EXPRESSION'
+    ],
+    
+    "ASSIGNMENT_SIMPLE": ['VARIABLE SPACE EQUALS SPACE EXPRESSION SEMICOLON NEW_LINE'],
+    "ASSIGNMENT_COMPLEX": [
+        'VARIABLE SPACE EQUALS SPACE SIMPLE_ARITHMETIC_EXPRESSION SEMICOLON NEW_LINE'
+        'VARIABLE SPACE EQUALS SPACE EXPRESSION SEMICOLON NEW_LINE'
+        ''
+    ],
+    
+    
+    # Levels of complexity
+    "LEVEL_1.1": ['IDENTIFIER_INITIALIZATION ASSIGNMENT_SIMPLE ADVANCED_DISPLAY'],
+    "LEVEL_1.2": ['IDENTIFIER_INITIALIZATION ASSIGNMENT_COMPLEX ADVANCED_DISPLAY'],
+    
+    "ALL": ['LEVEL_1.1', 'LEVEL_1.2'],
+}
